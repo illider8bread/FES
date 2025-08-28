c5128b85cabd038d78d4bfcecb648a6a33f85a78
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-08-28 12:32:27 -0600

    complete revamp of css

--- Javascript Mastery/app.js
+++ Javascript Mastery/app.js
@@ -1,8 +1,50 @@
-function welcomePersonToFES(name){
-    console.log("Welcome to FES, "+ name)
+function addition(a, b) {
+    return a + b;
 }
 
-welcomePersonToFES("David")
+console.log(addition(-3, -5));
 
-welcomePersonToFES('Mithri')
-welcomePersonToFES('Sarah')
+function hoursIntoSeconds(hours) {
+    return hours * 3600 
+}
+
+console.log(hoursIntoSeconds(24))
+
+function calcPerimeter(width, height) {
+    return (width * 2) + (height * 2);
+}
+
+console.log(calcPerimeter(2, 9))
+
+function calcTriangleArea(base, height) {
+    return 0.5 * base * height;
+}
+
+console.log(calcTriangleArea(20,20))
+
+function appendFrontend(string) {
+    return string + "Frontend";
+}
+
+console.log(appendFrontend("apple"))
+
+function sumGreaterThan100(num1, num2) {
+    let sum = num1 + num2;
+    return sum > 100;
+}
+
+console.log(sumGreaterThan100(110, 7))
+
+function oppositeBoolean(bool){
+    return !bool;
+}
+
+console.log(oppositeBoolean(true))
+
+function isNotZero(num){
+    return num !== 0;
+}
+
+console.log(isNotZero(7))
+
+return (num % 2) === 1;
