4d05258cb8f94679f9a7b8b35b5d68a829979c8a
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-08-31 16:49:09 -0600

    decided api, created filter options. commented in api links

--- JS/JS Final Project/script.js
+++ JS/JS Final Project/script.js
@@ -1,6 +1,5 @@
-function carAnimation(){
-
-}
+// API Key = http://www.omdbapi.com/?i=tt3896198&apikey=a4fe1dfc&
+//API Search = http://www.omdbapi.com/?i=tt3896198&apikey=a4fe1dfc&s=
 
 function contactMe(){
     alert("This feature has not yet been implemented");
