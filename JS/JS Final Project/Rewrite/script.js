d9089e6ae2937731c1b37fdc91196d55fdb67ae9
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-02 10:15:35 -0600

    completed final project!

--- JS/JS Final Project/Rewrite/script.js
+++ JS/JS Final Project/Rewrite/script.js
@@ -1,19 +1,28 @@
 // API = https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=20&has_breed=1
+const kittiesEl = document.querySelector(".kitties");
+const breedFilterEl = document.querySelector(".breed__filter");
 
 function notActive() {
     alert("this feature has not been implemented");
 }
 
-const kittiesEl = document.querySelector(".kitties");
-const breedFilterEl = document.querySelector(".breed__filter");
-
-async function renderCats(searchterm) {
-    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=20&has_breeds=true&size=small&breed_ids=${searchterm}`);
+async function renderCatsInitial() {
+    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=21&has_breeds=true&size=small`);
     const catsData = await cats.json();
-    console.log(catsData);
-    
-    kittiesEl.innerHTML = catsData.map(
-        (cat) => `
+    kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
+}
+
+async function renderCatsSearch(event) {
+    console.log(event);
+    const searchterm = event.target.value;
+    console.log(searchterm);
+    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=21&has_breeds=true&size=small&breed_ids=${searchterm}`);
+    const catsData = await cats.json();
+    kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
+}
+
+function catHtml(cat) {
+    return `
         <div class="kitty">
             <img src="${cat.url}" alt="Picture of ${cat.id}">
             <div class="kitty__text">
@@ -25,20 +34,16 @@ async function renderCats(searchterm) {
                 </p>
             </div>
         </div>`
-    ).join("");
 }
-
-renderCats();
 
 async function breedsList() {
     const breed = await fetch("https://api.thecatapi.com/v1/breeds");
     const breedData = await breed.json();
-
-    breedFilterEl.innerHTML = `<option value="" selected disabled>Breed:</option>
-    ` + breedData.map(
-        (breed) =>
-            `<option value="${breed.id}">${breed.name}</option>`
-        ).join("");
+    
+    breedFilterEl.innerHTML = `<option value="" selected disabled>Breeds</option>` 
+    + breedData.map((breed) => `<option value="${breed.id}">${breed.name}</option>`).join("");
 }
++
 
+document.addEventListener('DOMContentLoaded', renderCatsInitial(), false);
 breedsList();
