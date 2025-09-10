a99279be42f9a993569991e66a9b3e1821c8532e
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-10 13:22:48 -0600

    Creating new js file to change to omdb

--- JS/JS Final Project/Rewrite/script.js
+++ JS/JS Final Project/Rewrite/script.js
@@ -1,95 +1,103 @@
 // API = https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=20&has_breed=1
-const kittiesEl = document.querySelector(".kitties");
-const breedFilterEl = document.querySelector(".breed__filter");
-const breedListEl = document.querySelector(".kitty__breeds--list");
+// const kittiesEl = document.querySelector(".kitties");
+// const breedFilterEl = document.querySelector(".breed__filter");
+// const breedListEl = document.querySelector(".kitty__breeds--list");
 
-function notActive() {
-    alert("this feature has not been implemented");
-}
+// function notActive() {
+//     alert("this feature has not been implemented");
+// }
 
-async function renderCatsInitial(sortSelection, searchTerm) {
-    sortSelection = sortSelection || "none";
-    searchTerm = searchTerm || " ";
-    console.log(sortSelection);
-    console.log(searchTerm)
-    if (searchTerm != " "){
-    // run the search function here
-    const searchedCatID = findID(searchTerm);
-    const searchQuery = `&breed_ids=${searchedCatID}`
-    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC${searchQuery}`);
-    const catsData = await cats.json();
-    kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
-    return
-    } else if (searchTerm != " " && sortSelection != "none"){
-        const searchedCatID = findID(searchTerm);
-    const searchQuery = `&breed_ids=${searchedCatID}`
-    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC${searchQuery}`);
-    const catsData = await cats.json();
-    sortCats(sortSelection);
-    kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
-    return
-    } else {
-    const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC`);
-    const catsData = await cats.json();
-    kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
-    }
-}
+// async function renderCatsInitial(sortSelection, searchTerm) {
+//     sortSelection = sortSelection || "none";
+//     searchTerm = searchTerm || " ";
+//     console.log(sortSelection);
+//     console.log(searchTerm)
+//     if (searchTerm != " "){
+//     // run the search function here
+//     const searchedCatID = findID(searchTerm);
+//     const searchQuery = `&breed_ids=${searchedCatID}`
+//     const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC${searchQuery}`);
+//     const catsData = await cats.json();
+//     kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
+//     return
+//     } else if (searchTerm != " " && sortSelection != "none"){
+//         const searchedCatID = findID(searchTerm);
+//     const searchQuery = `&breed_ids=${searchedCatID}`
+//     const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC${searchQuery}`);
+//     const catsData = await cats.json();
+//     sortCats(sortSelection);
+//     kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
+//     return
+//     } else {
+//     const cats = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_eMv9HahQB2nEhfURSsyvpJJXfiLtVlgBazYtMuWuONPkBWk5ebCeS3lRkd0MnuFe&limit=24&has_breeds=true&order=ASC`);
+//     const catsData = await cats.json();
+//     kittiesEl.innerHTML = catsData.map((cat) => catHtml(cat)).join("");
+//     }
+// }
 
-function sortShownCats(event) {
-    renderCatsInitial(event.target.value);
-}
+// function sortShownCats(event) {
+//     renderCatsInitial(event.target.value);
+// }
 
-function sortCats(sortSelection){
-if (sortSelection === 'none'){
-    } else if (sortSelection === 'aff_low_to_high'){
-        catsData.sort((a, b)=> a['breeds']['0']['affection_level'] - b['breeds']['0']['affection_level']);
-    } else if (sortSelection === 'aff_high_to_low'){
-        catsData.sort((a, b)=> b['breeds']['0']['affection_level'] - a['breeds']['0']['affection_level']);
-    } else if (sortSelection === 'en_low_to_high'){
-        catsData.sort((a, b)=> a['breeds']['0']['energy_level'] - b['breeds']['0']['energy_level']);
-    } else if (sortSelection === 'en_high_to_low'){
-        catsData.sort((a, b)=> b['breeds']['0']['energy_level'] - a['breeds']['0']['energy_level']);
-    } else if (sortSelection === 'int_low_to_high'){
-        catsData.sort((a, b)=> a['breeds']['0']['energy_level'] - b['breeds']['0']['energy_level']);
-    } else if (sortSelection === 'int_high_to_low'){
-        catsData.sort((a, b)=> b['breeds']['0']['energy_level'] - a['breeds']['0']['energy_level']);
-    }
-}
+// function sortCats(sortSelection){
+// if (sortSelection === 'none'){
+//     } else if (sortSelection === 'aff_low_to_high'){
+//         catsData.sort((a, b)=> a['breeds']['0']['affection_level'] - b['breeds']['0']['affection_level']);
+//     } else if (sortSelection === 'aff_high_to_low'){
+//         catsData.sort((a, b)=> b['breeds']['0']['affection_level'] - a['breeds']['0']['affection_level']);
+//     } else if (sortSelection === 'en_low_to_high'){
+//         catsData.sort((a, b)=> a['breeds']['0']['energy_level'] - b['breeds']['0']['energy_level']);
+//     } else if (sortSelection === 'en_high_to_low'){
+//         catsData.sort((a, b)=> b['breeds']['0']['energy_level'] - a['breeds']['0']['energy_level']);
+//     } else if (sortSelection === 'int_low_to_high'){
+//         catsData.sort((a, b)=> a['breeds']['0']['energy_level'] - b['breeds']['0']['energy_level']);
+//     } else if (sortSelection === 'int_high_to_low'){
+//         catsData.sort((a, b)=> b['breeds']['0']['energy_level'] - a['breeds']['0']['energy_level']);
+//     }
+// }
 
-function catHtml(cat) {
-    return `
-        <div class="kitty ${cat['breeds']['0']['id']}">
-            <img src="${cat.url}" alt="Picture of ${cat.id}">
-            <div class="kitty__text ">
-                <p class="kitty__breed">
-                <b>Breed:</b> ${cat['breeds']['0']['name']}
-                </p>
-                    <div class="kitty__stats">
-                    <div class="aff kitty__stat">Affection <br> ${cat['breeds']['0']['affection_level']}</div>
-                    <div class="en kitty__stat"> Energy <br> ${cat['breeds']['0']['energy_level']}</div>
-                    <div class="int kitty__stat"> Intelligence <br> ${cat['breeds']['0']['energy_level']}</div>
-                </div>
-            </div>
-        </div>`
-}
+// function catHtml(cat) {
+//     return `
+//         <div class="kitty ${cat['breeds']['0']['id']}">
+//             <img src="${cat.url}" alt="Picture of ${cat.id}">
+//             <div class="kitty__text ">
+//                 <p class="kitty__breed">
+//                 <b>Breed:</b> ${cat['breeds']['0']['name']}
+//                 </p>
+//                     <div class="kitty__stats">
+//                     <div class="aff kitty__stat">Affection <br> ${cat['breeds']['0']['affection_level']}</div>
+//                     <div class="en kitty__stat"> Energy <br> ${cat['breeds']['0']['energy_level']}</div>
+//                     <div class="int kitty__stat"> Intelligence <br> ${cat['breeds']['0']['energy_level']}</div>
+//                 </div>
+//             </div>
+//         </div>`
+// }
 
 // if searchinput = map.name then return ID then 
 
 async function findID(searchTerm){
+    console.log(searchTerm);
     const breed = await fetch("https://api.thecatapi.com/v1/breeds");
     const breedData = await breed.json();
+    console.log(breedData);
     const breedID = breedData.map((breed) => {
-        console.log(breed)
-        const searchBreed = `${breed['breeds']['0']['name']}`;
-        console.log(searchBreed);
-        // if (breed['breeds']['0']['name'] == searchTerm){
-        //     return breed['breeds']['0']['id'];
+    const searchBreed = breed.name;
+    console.log(searchBreed)
+
+    for (let i = 0; i  < breedData.length; ++) {
+        
+        
+    }
+        // if (breed.name == searchTerm){
+        //     console.log(breed.id);
+        //      return breed.id;}
         // } else{
         //     alert("There were no results matching your search. Please try again. If you need help, reference the lists of breeds available.")
         // }
-    })
+    }).join("");
 }
 
+findID("Bengal");
 
 async function renderBreeds() {
      const breed = await fetch("https://api.thecatapi.com/v1/breeds");
