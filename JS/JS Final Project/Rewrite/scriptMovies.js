13697ae893fb68c4eaa6378be5a3ca40eaff6500
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-09-10 14:42:42 -0600

    Created alternate that uses OMDb

---
+++ JS/JS Final Project/Rewrite/scriptMovies.js
@@ -0,0 +1,58 @@
+// OMDb API link with key= http://www.omdbapi.com/?i=tt3896198&apikey=e44d38c3
+const kittiesEl = document.querySelector(".kitties");
+
+async function renderCatsInitial(sortSelection, searchTerm) {
+    sortSelection = sortSelection || "none";
+    searchTerm = searchTerm ||"Star Wars";
+
+    const searchTermPlussed = searchTerm.replace(/ /g, '+');
+    const searchQuery = `&s=${searchTermPlussed}`;
+
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e44d38c3${searchQuery}`);
+        const moviesData = await response.json();
+
+        // Check if the response contains the Search array
+        if (moviesData.Search) {
+            if (sortSelection === 'release_low_to_high') {
+                moviesData.Search.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+            } else if (sortSelection === 'release_high_to_low') {
+                moviesData.Search.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+            }
+            kittiesEl.innerHTML = moviesData.Search.map(movieHtml).join('');
+        } else {
+            kittiesEl.innerHTML = '<p>No movies found.</p>';
+        }
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        kittiesEl.innerHTML = '<p>Error fetching movies. Please try again later.</p>';
+    }
+}
+
+function movieHtml(movie) {
+    return `
+        <div class="kitty">
+            <img src="${movie.Poster}" alt="Picture of ${movie.Title}">
+            <div class="kitty__text ">
+                <p class="kitty__breed--header">
+                ${movie.Title}
+                </p>
+                <p class="kitty__breed">
+                <b>Release Year: </b> ${movie.Year}
+                </p>
+            </div>
+        </div>`;
+}
+
+function sortCats(event) {
+    let sort = event.target.value;
+    let search = document.getElementById(`search__input`).value;
+    renderCatsInitial(sort, search);
+}
+
+function searchCats(){
+    let search = document.getElementById(`search__input`).value;
+    renderCatsInitial("none", search);
+}
+
+document.addEventListener('DOMContentLoaded', renderCatsInitial(), false);
